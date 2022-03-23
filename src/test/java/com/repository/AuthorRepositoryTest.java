e0c26bed628e386f4c63f9fe28e0f543d90ec824
flyguy-ty <truperti17@gmail.com>
2022-03-23 14:04:38 -0400

    ...

---
+++ src/test/java/com/repository/AuthorRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.repository;
+
+import com.model.Author;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AuthorRepositoryTest {
+
+    @Autowired
+    private AuthorRepository underTest;
+
+    @Test
+    void findById() {
+
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAllUserReviews() {
+    }
+
+    @Test
+    void findByUserName() {
+        //given
+        Author author = new Author();
+        author.setPassWord("password");
+        author.setUserName("user");
+        underTest.save(author);
+
+        //when
+        Author exists = underTest.findByUserName("user");
+
+        //Then
+        assertSame("password", exists.getPassWord());
+    }
+}
